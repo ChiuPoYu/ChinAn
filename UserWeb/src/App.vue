d4aeb71d401992229c9e1818e502e5f16f2deda4
albert900617 <albert900617@gmail.com>
2025-09-21 22:48:18 +0800

    建立初版程式

---
+++ UserWeb/src/App.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="container">
+    <h1>🚗 車輛維護系統</h1>
+    <MaintenanceForm @addRecord="addRecord" />
+    <h2>維護紀錄</h2>
+    <ul>
+      <li v-for="(record, index) in records" :key="index">
+        {{ record.date }} - {{ record.vehicle }} - {{ record.task }}
+        <button @click="deleteRecord(index)">刪除</button>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import MaintenanceForm from './components/MaintenanceForm.vue'
+
+export default {
+  components: { MaintenanceForm },
+  data() {
+    return {
+      records: []
+    }
+  },
+  methods: {
+    addRecord(record) {
+      this.records.push(record)
+    },
+    deleteRecord(index) {
+      this.records.splice(index, 1)
+    }
+  }
+}
+</script>
+
+<style>
+.container {
+  max-width: 600px;
+  margin: auto;
+  font-family: sans-serif;
+}
+button {
+  margin-left: 10px;
+}
+</style>
---
+++ UserWeb/src/components/MaintenanceForm.vue
@@ -0,0 +1,32 @@
+<template>
+  <form @submit.prevent="submitForm">
+    <input v-model="vehicle" placeholder="車輛名稱" required />
+    <input v-model="task" placeholder="維護項目" required />
+    <input v-model="date" type="date" required />
+    <button type="submit">新增紀錄</button>
+  </form>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      vehicle: '',
+      task: '',
+      date: ''
+    }
+  },
+  methods: {
+    submitForm() {
+      this.$emit('addRecord', {
+        vehicle: this.vehicle,
+        task: this.task,
+        date: this.date
+      })
+      this.vehicle = ''
+      this.task = ''
+      this.date = ''
+    }
+  }
+}
+</script>
